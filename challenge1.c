c54dac447b5ce01cd56f6eac305c7498b7e70151
fakhreddinelargou <largoufakhreddine@gmail.com>
2025-09-02 10:47:47 +0100

    challenge-1

---
+++ challenge1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    
+
+    
+    
+  char  nom[] = "largou";
+   char  prenom[] = "fakhreddine";
+   char  email[] = "largofakhrddin@gmail.com";
+   int  age = 20;
+   char  sexe[] = "male" ;
+    
+    printf("%s \n %s  \n %s  \n %d  \n %s ",nom,prenom,email,age,sexe);
+
+    return 0;
+}
