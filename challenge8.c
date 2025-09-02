da45b8093f5475bb1f76c032860bf5bd5d3e88cc
fakhreddinelargou <largoufakhreddine@gmail.com>
2025-09-02 17:09:11 +0100

    new challenges

---
+++ challenge8.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    
+    
+    
+int i , n , t;
+
+printf("enter number : ");
+scanf("%d",& n);
+
+t = 0;
+for(i=1 ; i <= n ; i++){
+    t = t + i;
+}
+
+    printf(" %d \n", t);
+
+
+
+
+    return 0;
+}
